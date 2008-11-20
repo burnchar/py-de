5995e721c451cc7d1e8623ba062df8f115613018
yoann.buch <yoann.buch@84c433d8-a69d-11dd-91bf-632da8b6e10b>
2008-11-20 17:56:01 +0000

    More work on pydeTemplates Worked on a function to get a list of the templates name of the selected language.

--- pydeTemplates.py
+++ pydeTemplates.py
@@ -3,23 +3,48 @@
 from PyQt4 import *
 from PyQt4.QtCore import *
 
-
+sLine = ""
 class pydeTemplates:
-  def test(self):
-    file = QtCore.QFile("templates")	
+      
+  def getTemplatesListOf(self,language):    
+    file = QtCore.QFile(self.filename)	
     if( file.open( QIODevice.ReadOnly) ): 
-      ts = QtCore.QTextStream(file)
-      bip = ts.read()
-      #a = file.readData(10)
-      print a
-      file.close()
-  def __init__(self):
-    pass	    
+      self.ts = QtCore.QTextStream(file)   
+     
+     
+    listTemplates = "" #use QStringList instead
+    load = False
+    while True:
+	line = str(self.ts.readLine())     
+	if line == "":
+	  break	  
+	
+        if line.find("language") != -1:
+	  #new language
+	  #print line.split()[1]
+	  if line.split()[1] == language:
+	    load = True
+	  else:
+	    load = False
+        elif line.find("name") != -1:	
+	  #new template  
+	  #print line.split(" ", 1)[1] 
+	  if load == True:
+	   listTemplates += line.split(" ", 1)[1]
+		  
+        else:
+          pass  
+	
+    file.close()
+    return listTemplates
+
+  def __init__(self, filename):
+    self.filename = filename
   	
 	
 if __name__ == '__main__':
-  a = myTemplates()
-  a.test()	
+  a = pydeTemplates("templates")
+  a.getTemplatesListOf("py")	
 
  
 
