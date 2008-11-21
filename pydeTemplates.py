135e24b58ddd4157b763bf5d5cccbc04c1dd16c7
yoann.buch <yoann.buch@84c433d8-a69d-11dd-91bf-632da8b6e10b>
2008-11-21 22:55:00 +0000

    Template module done. Can load cpp, python and fortran template.
    Next step could be to implement add/remove templates functionnalities

--- pydeTemplates.py
+++ pydeTemplates.py
@@ -8,36 +8,50 @@ class pydeTemplates:
       
   def getTemplatesListOf(self,language):    
     file = QtCore.QFile(self.filename)	
+    listTemplates = QtCore.QStringList()
     if( file.open( QIODevice.ReadOnly) ): 
-      self.ts = QtCore.QTextStream(file)   
-     
-     
-    listTemplates = "" #use QStringList instead
-    load = False
-    while True:
-	line = str(self.ts.readLine())     
-	if line == "":
+      ts = QtCore.QTextStream(file)   
+
+      while True:
+  	line = str(ts.readLine())     
+     	if line == "":
+	  #end of file
 	  break	  
+        if line.find("<" + language + ">") != -1:
+	  #right language
+	  #get template name
+	  listTemplates.append(line.split(" ", 1)[1])
 	
-        if line.find("language") != -1:
-	  #new language
-	  #print line.split()[1]
-	  if line.split()[1] == language:
-	    load = True
-	  else:
-	    load = False
-        elif line.find("name") != -1:	
-	  #new template  
-	  #print line.split(" ", 1)[1] 
-	  if load == True:
-	   listTemplates += line.split(" ", 1)[1]
-		  
-        else:
-          pass  
-	
-    file.close()
+      file.close()
     return listTemplates
 
+  def loadTemplates(self, language, template):
+    file = QtCore.QFile(self.filename)	
+    templateContent = ""
+    if( file.open( QIODevice.ReadOnly) ): 
+      ts = QtCore.QTextStream(file)  
+      while True:
+        line = str(ts.readLine())     
+	if line.find("<" + language + ">") != -1:
+	  #first check right language
+	  if line.split(" ", 1)[0].strip("<>") == language:
+	    #second, check right template
+	    if line.split(" ", 1)[1] == template:
+	      #read and store template    
+              while True:
+	        line = str(ts.readLine()) 
+	        if line.find("<end>") != -1:
+		  break
+	        else:
+		  templateContent += line + "\n"
+	      #template read, leave file reading
+              break
+	if line == "":
+	  #end of file
+	  break	     
+      
+    return templateContent
+  
   def __init__(self, filename):
     self.filename = filename
   	
